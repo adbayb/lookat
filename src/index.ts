5111cc39cde15743a3676c6f975f394bac6cdb94
Ayoub Adib <adbayb@gmail.com>
2020-10-31 22:17:12 +0100

    refactor: reduce lib size by using function instead of class for ProxyHandler

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ type Context = {
 	proxies: WeakMap<Target, Target>;
 };
 
-export const context: Context = {
+const context: Context = {
 	currentObserver: null,
 	observers: new WeakMap(),
 	proxies: new WeakMap(),
@@ -28,112 +28,105 @@ const isNativePropertyKey = (key: PropertyKey) => {
 	);
 };
 
-class ObservableHandler<Value extends Record<string, unknown>>
-	implements ProxyHandler<Value> {
-	eventHandlers?: ObservableHandlers;
+const createProxyHandler = (
+	eventHandlers?: ObservableHandlers
+): ProxyHandler<Target> => {
+	return {
+		get(...args) {
+			const { currentObserver } = context;
+			const target = args[0];
+			const key = args[1] as string;
+			const value = target[key];
 
-	constructor(eventHandlers?: ObservableHandlers) {
-		this.eventHandlers = eventHandlers;
-	}
+			// console.log(`get->${key}`, target, target[key]);
 
-	get(...args: Parameters<NonNullable<ProxyHandler<Value>["get"]>>) {
-		const { currentObserver } = context;
-		const target = args[0];
-		const key = args[1] as string;
-		const value = target[key];
+			if (
+				currentObserver &&
+				// @note: we do not observer native built-in function (since stable in normal conditions)
+				!(isNativePropertyKey(key) && typeof target[key] === "function")
+			) {
+				const callbacks = context.observers.get(target) || {};
+				// @note: we map current observer to all traversed properties (not only the last accessed property)
+				// to allow nested observers to be notified in case of parent properties reset.
+				// For example, if we have following observable shape: person = { firstName: "Ayoub", age: 28 }
+				// If we reset its nested values via person.$ = {}, We expect that observers associated to "firstName" and "age" property are called.
+				// Mapping also their observers to the $ parent property allows to call their associated `observe` callbacks and
+				// recompute their new observers dependency list to track future updates:
+				const propertyCallbacks = callbacks[key] || [];
 
-		// console.log(`get->${key}`, target, target[key]);
+				if (!propertyCallbacks.includes(currentObserver)) {
+					propertyCallbacks.push(currentObserver);
+					callbacks[key] = propertyCallbacks;
 
-		if (
-			currentObserver &&
-			// @note: we do not observer native built-in function (since stable in normal conditions)
-			!(isNativePropertyKey(key) && typeof target[key] === "function")
-		) {
-			const callbacks = context.observers.get(target) || {};
-			// @note: we map current observer to all traversed properties (not only the last accessed property)
-			// to allow nested observers to be notified in case of parent properties reset.
-			// For example, if we have following observable shape: person = { firstName: "Ayoub", age: 28 }
-			// If we reset its nested values via person.$ = {}, We expect that observers associated to "firstName" and "age" property are called.
-			// Mapping also their observers to the $ parent property allows to call their associated `observe` callbacks and
-			// recompute their new observers dependency list to track future updates:
-			const propertyCallbacks = callbacks[key] || [];
-
-			if (!propertyCallbacks.includes(currentObserver)) {
-				propertyCallbacks.push(currentObserver);
-				callbacks[key] = propertyCallbacks;
-
-				context.observers.set(target, callbacks);
-			}
-		}
-
-		if (typeof this.eventHandlers?.onGet === "function") {
-			this.eventHandlers.onGet(target, key, value);
-		}
-
-		return isObject(value)
-			? proxify(value, this.eventHandlers)
-			: Reflect.get(...args);
-	}
-
-	set(...args: Parameters<NonNullable<ProxyHandler<Value>["set"]>>) {
-		const target = args[0];
-		const key = args[1] as string | number;
-		const oldValue = target[key];
-		// @note: we mutate before notifying to let observers get mutated value
-		const result = Reflect.set(...args);
-		const newValue = target[key];
-		const needsUpdate = isNativePropertyKey(key)
-			? // @note: some set trap are called too late after their mutation
-			  // for example, when a new item is pushed in the array, the set handler for the new item addition is triggered
-			  // and, in the same time that the new value is added, the `length` property is also updated (at least in chrome) without waiting that its handler is called.
-			  // Only after the set handler for `length` is triggered. The issue is that when the `length` set trap is called, the old value corresponds to the new one
-			  // => the needsUpdate flag is set to false resulting in a no-op update for observers watching the `length` property updates
-			  // (for example, iterators like forEach but also other native array methods that relies on length property to manage its logic).
-			  // To solve it, we exclude native property from the `needsUpdate` flag evaluation: an update will be always needed for native property key (Object and Array).
-			  // It should not give false positives since native property key setters might always come from a mutation but we need to be vigilant about this potential issue source...
-			  true
-			: oldValue !== newValue;
-		const observers = context.observers.get(target)?.[key];
-
-		// console.warn(`set->${key}`, ...args);
-
-		if (needsUpdate) {
-			if (typeof this.eventHandlers?.onSet === "function") {
-				this.eventHandlers.onSet(target, key, oldValue, newValue);
+					context.observers.set(target, callbacks);
+				}
 			}
 
-			if (observers) {
-				observers.forEach((observer) => observer());
+			if (typeof eventHandlers?.onGet === "function") {
+				eventHandlers.onGet(target, key, value);
 			}
-		}
 
-		return result;
-	}
+			return isObject(value)
+				? proxify(value, eventHandlers)
+				: Reflect.get(...args);
+		},
+		set(...args) {
+			const target = args[0];
+			const key = args[1] as string | number;
+			const oldValue = target[key];
+			// @note: we mutate before notifying to let observers get mutated value
+			const result = Reflect.set(...args);
+			const newValue = target[key];
+			const needsUpdate = isNativePropertyKey(key)
+				? // @note: some set trap are called too late after their mutation
+				  // for example, when a new item is pushed in the array, the set handler for the new item addition is triggered
+				  // and, in the same time that the new value is added, the `length` property is also updated (at least in chrome) without waiting that its handler is called.
+				  // Only after the set handler for `length` is triggered. The issue is that when the `length` set trap is called, the old value corresponds to the new one
+				  // => the needsUpdate flag is set to false resulting in a no-op update for observers watching the `length` property updates
+				  // (for example, iterators like forEach but also other native array methods that relies on length property to manage its logic).
+				  // To solve it, we exclude native property from the `needsUpdate` flag evaluation: an update will be always needed for native property key (Object and Array).
+				  // It should not give false positives since native property key setters might always come from a mutation but we need to be vigilant about this potential issue source...
+				  true
+				: oldValue !== newValue;
+			const observers = context.observers.get(target)?.[key];
 
-	deleteProperty(
-		...args: Parameters<NonNullable<ProxyHandler<Value>["deleteProperty"]>>
-	) {
-		const target = args[0];
-		const key = args[1] as string | number;
-		const result = Reflect.deleteProperty(...args);
-		const targetObservers = context.observers.get(target);
+			// console.warn(`set->${key}`, ...args);
 
-		if (typeof this.eventHandlers?.onDeleteProperty === "function") {
-			this.eventHandlers.onDeleteProperty(target, key);
-		}
+			if (needsUpdate) {
+				if (typeof eventHandlers?.onSet === "function") {
+					eventHandlers.onSet(target, key, oldValue, newValue);
+				}
 
-		if (targetObservers) {
-			const observers = targetObservers?.[key];
-
-			if (observers) {
-				observers.forEach((observer) => observer());
-				delete targetObservers[key];
+				if (observers) {
+					observers.forEach((observer) => observer());
+				}
 			}
-		}
 
-		return result;
-	}
-}
+			return result;
+		},
+		deleteProperty(...args) {
+			const target = args[0];
+			const key = args[1] as string | number;
+			const result = Reflect.deleteProperty(...args);
+			const targetObservers = context.observers.get(target);
+
+			if (typeof eventHandlers?.onDeleteProperty === "function") {
+				eventHandlers.onDeleteProperty(target, key);
+			}
+
+			if (targetObservers) {
+				const observers = targetObservers?.[key];
+
+				if (observers) {
+					observers.forEach((observer) => observer());
+					delete targetObservers[key];
+				}
+			}
+
+			return result;
+		},
+	};
+};
 
 const createObserver = (callback: VoidFunction): Observer => {
 	const observer = () => {
@@ -171,7 +164,7 @@ const proxify = (
 		return storedProxy;
 	}
 
-	const proxy = new Proxy(target, new ObservableHandler(eventHandlers));
+	const proxy = new Proxy(target, createProxyHandler(eventHandlers));
 
 	context.proxies.set(target, proxy);
 
@@ -179,6 +172,26 @@ const proxify = (
 };
 
 type ObservableSource<Value> = Value | (() => Value);
+
+/*
+type ObservableHandlers = {
+	onMutate?: (type: "set" | "delete", target, targetKeys: PropertyKey[])
+}
+
+type ObservableLifecycleListener = {
+	create
+	read
+	update
+	delete
+}
+
+type ObservableHooks = {
+	beforeMutation
+	afterMutation
+	beforeGet
+	afterGet
+}
+*/
 
 type ObservableHandlers = {
 	onSet?: (
@@ -194,7 +207,7 @@ type ObservableHandlers = {
 export const createObservable = <Value>(
 	value: ObservableSource<Value>,
 	eventHandlers?: ObservableHandlers
-): Observable<Value> => {
+) => {
 	const observableValue = proxify(
 		{ $: typeof value !== "function" ? value : undefined },
 		eventHandlers
